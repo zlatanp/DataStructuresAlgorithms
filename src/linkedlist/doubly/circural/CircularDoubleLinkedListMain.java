5d6110b00ea8fcf5d193a8fea97bb41c4757aebd
Zlatan <zlatanprecanica@gmail.com>
2022-01-07 21:17:34 +0000

    Circural Double Linked List

---
+++ src/linkedlist/doubly/circural/CircularDoubleLinkedListMain.java
@@ -0,0 +1,14 @@
+package linkedlist.doubly.circural;
+
+class CircularDoubleLinkedListMain {
+    public static void main(String[] args) {
+        CircularDoubleLinkedList cdll = new CircularDoubleLinkedList();
+        cdll.createCDLL(1);
+        cdll.insertNode(2, 0);
+        cdll.insertNode(3, 2);
+        cdll.insertNode(4, 5);
+        cdll.traverseCDLL();
+        cdll.deleteCDLL();
+        cdll.traverseCDLL();
+    }
+}
---
+++ src/linkedlist/doubly/circural/DoubleNode.java
@@ -0,0 +1,7 @@
+package linkedlist.doubly.circural;
+
+public class DoubleNode {
+    public int value;
+    public DoubleNode next;
+    public DoubleNode prev;
+}
