42b36c7052cfa4fc38bc6c046437ad4cb7a318d2
Zlatan <zlatanprecanica@gmail.com>
2022-01-08 21:04:41 +0000

    Graph

---
+++ src/graph/adjacencymatrix/GraphNode.java
@@ -0,0 +1,12 @@
+package graph.adjacencymatrix;
+
+public class GraphNode {
+    public String name;
+    public int index;
+    public boolean isVisited = false;
+
+    public GraphNode(String name, int index) {
+        this.name = name;
+        this.index = index;
+    }
+}
