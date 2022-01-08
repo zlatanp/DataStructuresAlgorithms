0be878f06384d99dc707b1e3b155bbdc8e34f266
Zlatan <zlatanprecanica@gmail.com>
2022-01-08 20:36:57 +0000

    Double Hashing

---
+++ src/hashing/doublehashing/DoubleHashingMain.java
@@ -0,0 +1,14 @@
+package hashing.doublehashing;
+
+class DoubleHashingMain {
+    public static void main(String[] args) {
+        DoubleHashing doubleHashing = new DoubleHashing(13);
+        doubleHashing.insertKeyInHashTable("The");
+        doubleHashing.insertKeyInHashTable("quick");
+        doubleHashing.insertKeyInHashTable("brown");
+        doubleHashing.insertKeyInHashTable("fox");
+        doubleHashing.insertKeyInHashTable("over");
+        doubleHashing.insertKeyInHashTable("lazy");
+        doubleHashing.displayHashTable();
+    }
+}
