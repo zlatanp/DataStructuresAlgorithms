7e504d351698ae8a8b1d9afd2968e60faa9b34b8
Zlatan <zlatanprecanica@gmail.com>
2022-01-08 20:40:42 +0000

    Hashing Linear Probing

---
+++ src/hashing/linearprobing/LinearProbingMain.java
@@ -0,0 +1,14 @@
+package hashing.linearprobing;
+
+class LinearProbingMain {
+    public static void main(String[] args) {
+        LinearProbing linearProbing = new LinearProbing(13);
+        linearProbing.insertInHashTable("The");
+        linearProbing.insertInHashTable("quick");
+        linearProbing.insertInHashTable("brown");
+        linearProbing.insertInHashTable("fox");
+        linearProbing.insertInHashTable("over");
+        linearProbing.deleteKeyHashTable("fox");
+        linearProbing.displayHashTable();
+    }
+}
