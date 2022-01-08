c537e38d9e85d090972a466a1f0d3588649dd10e
Zlatan <zlatanprecanica@gmail.com>
2022-01-08 20:42:28 +0000

    Hashing Quadratic Probing

---
+++ src/hashing/quadraticprobing/QuadraticProbingMain.java
@@ -0,0 +1,15 @@
+package hashing.quadraticprobing;
+
+class QuadraticProbingMain {
+    public static void main(String[] args) {
+        QuadraticProbing quadraticProbing = new QuadraticProbing(13);
+        quadraticProbing.insertKeyInHashTable("The");
+        quadraticProbing.insertKeyInHashTable("quick");
+        quadraticProbing.insertKeyInHashTable("brown");
+        quadraticProbing.insertKeyInHashTable("fox");
+        quadraticProbing.insertKeyInHashTable("over");
+        quadraticProbing.insertKeyInHashTable("lazy");
+        quadraticProbing.displayHashTable();
+
+    }
+}
