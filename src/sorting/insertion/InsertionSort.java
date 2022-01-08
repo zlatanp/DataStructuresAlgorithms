06a77d7dea1505f5212c10e62eb888dc00f2b8f9
Zlatan <zlatanprecanica@gmail.com>
2022-01-08 20:50:00 +0000

    Add sorting

---
+++ src/sorting/insertion/InsertionSort.java
@@ -0,0 +1,21 @@
+package sorting.insertion;
+
+public class InsertionSort {
+    void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int temp = arr[i], j = i;
+            while (j > 0 && arr[j - 1] > temp) {
+                arr[j] = arr[j - 1];
+                j--;
+            }
+            arr[j] = temp;
+        }
+    }
+
+
+    public void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + "  ");
+        }
+    }
+}
