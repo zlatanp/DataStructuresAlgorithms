cd278f0afb66cebc2928ea01cf59522b7908084d
Zlatan <zlatanprecanica@gmail.com>
2022-01-08 20:55:38 +0000

    Searching

---
+++ src/searching/SearchingMain.java
@@ -0,0 +1,8 @@
+package searching;
+
+class SearchingMain {
+    public static void main(String[] args) {
+        int[] arr = {8, 9, 12, 15, 17, 19, 20, 21, 28};
+        Searching.binarySearch(arr, 20);
+    }
+}
